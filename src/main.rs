ced5f112800801ebb143cb5acbe376b26b0efe29
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-07-31 23:53:38 +0200

    2023-07-31@23-53-37

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+// ============ IMPORTS ============
 // Logger
 extern crate pretty_env_logger;
 #[macro_use]
 #[allow(unused_imports)]
 extern crate log;
 
-// ========== MAIN FUNCTION ==========
-fn main() {
-    // initialize Logger
-    pretty_env_logger::init_timed();
+// ======= STRUCTS AND ENUMS =======
 
-    // ===== MAIN CODE =====
+// ========= MAIN FUNCTION =========
+
+fn main() {
+    setup();
 
     println!("Hello, world!");
+}
+
+// =========== FUNCTIONS ===========
+// Setup function
+fn setup() {
+    // initialize Logger
+    pretty_env_logger::init_timed();
 }
