0a296b0f9034cac0fff333341dedfbe81e9d4c28
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-12-03 22:12:00 +0100

    add astralib to template

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 #[allow(unused_imports)]
 use tracing::*;
 
+// Import astralib (use `cargo update` to download library)
+#[allow(unused_imports)]
+extern crate astralib;
+
 fn main() {
     // Initialize tracing (Logger)
     tracing_subscriber::fmt::init();
